af93b30e6cb5df2a664bf83d4fa09a1687ff19c6
JustManForever <70839428+JustManForever@users.noreply.github.com>
2020-10-02 17:25:09 +0400

    feat: add code

--- src/index.js
+++ src/index.js
@@ -1,12 +1,42 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min =  function min(array) {
+    if ( arguments.length === 0 || array.length === 0 ) {
+        return 0;
+    }
+
+    let minv = array[0]
+
+    for (let key of array) {
+        if(key < minv) minv = key;
+    }
+
+    return minv;
+ }
+
+exports.max = function max(array) {
+    if ( arguments.length === 0 || array.length === 0 ) {
+        return 0;
+    }
+
+    let maxv = array[0]
+
+    for (let key of array) {
+        if(key > maxv) maxv = key;
+    }
+
+    return maxv;
 }
 
-exports.max = function max (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+    if ( arguments.length === 0 || array.length === 0 ) {
+        return 0;
+    }
 
-exports.avg = function avg (array) {
-  return 0;
+    let sum = 0
+
+    for (let key of array) {
+        sum += key;
+    }
+
+    return sum / array.length;
 }
